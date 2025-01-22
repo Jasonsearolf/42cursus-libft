d4b7c3d64e5991ee7c77324e983f330c32674a81
Jeinson Flores Castro <jeflores@c2r3s1.42madrid.com>
2025-01-22 21:56:34 +0100

    wed 22 day end

--- ft_bzero.c
+++ ft_bzero.c
@@ -6,7 +6,29 @@
 /*   By: jeflores <jeflores@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/22 21:18:34 by jeflores          #+#    #+#             */
-/*   Updated: 2025/01/22 21:18:37 by jeflores         ###   ########.fr       */
+/*   Updated: 2025/01/22 21:55:33 by jeflores         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+void	ft_bzero(void *ptr, size_t n)
+{
+	unsigned char	*p;
+	
+	p = ptr;
+	while (n--)
+	{
+		*p = 0;
+		p++;
+	}
+}
+
+int	main(void)
+{
+	char str[] = "Pikachu";
+	size_t n = 4;
+	ft_bzero(str, n);
+	printf("Ahora es %s", str);
+	return (0);
+}
