87d2ddd0345ed92c1b28fc79a46f2140a4a14015
Jason <jason@adatier.com>
2025-01-28 13:12:12 +0100

    corregir strlcpy revisar en chatgpt

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -12,24 +12,29 @@
 
 #include "libft.h"
 
-/* size_t ft_strlcpy(char *dst, const char *src, size_t n)
+size_t ft_strlcpy(char *dst, const char *src, size_t n)
 {
+	size_t i;
 
-	return (dst);
-} */
+	i = 0;
+	while (i <= n)
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	dst[i] = '\0';
+	return (i);
+}
 
 int main(void)
 {
 	char src[] = "Pikachu";
-	char dst[4];
+	char dst[5];
 
-	/* ft_strlcpy (dst, src, 4);
+	ft_strlcpy (dst, src, 5);
+	int result = (unsigned int) ft_strlcpy (dst, src, 5);
 	printf ("Destino: %s\n", dst);
-	printf ("Longitud: %zu"); */
-
-	strlcpy(dst, src, 4);
-	int result = strlcpy (dst, src, 4);
-	printf ("Destino: %s\n", dst); 
 	printf ("Longitud: %d", result);
+
 	return (0);
 }
