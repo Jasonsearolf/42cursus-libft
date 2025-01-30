b38f10eaf9081aab8b1bab799a5939132a581a21
Jason Searolf <jason@adatier.com>
2025-01-30 01:39:18 +0100

    home strlcat

---
+++ ft_strlcat.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jeflores <jeflores@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/29 18:34:34 by jeflores          #+#    #+#             */
+/*   Updated: 2025/01/30 01:14:21 by jeflores         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t ft_strlcat(char *dst, const char *src, size_t n)
+{
+	size_t srclen;
+	size_t dstlen;
+
+	size_t i;
+
+	srclen = ft_strlen (src);
+	dstlen = ft_strlen (dst);
+	i = 0;
+	if (n <= dstlen)
+	{
+		return (n + srclen);
+	}
+	while(i < (n - dstlen - 1) && src[i])
+	{
+		dst[dstlen + i] = src[i];
+		i++;
+	}
+	dst[dstlen + i] = '\0';
+	return (dstlen + srclen);
+}
+
+int main(void)
+{
+	char src[] = "chu";
+	char dst[] = "Pika";
+
+	int result = (unsigned int) ft_strlcat (dst, src, 20);
+	printf ("Destino: %s\n", dst);
+	printf ("Longitud: %d", result); 
+
+	return (0);
+}
