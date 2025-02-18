188c58dcc2a2746b9a0d2eae13a76a8e155bca51
Jason Searolf <jason@adatier.com>
2025-02-18 22:57:32 +0100

    split done

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: jeflores <jeflores@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/17 20:55:11 by jeflores          #+#    #+#             */
-/*   Updated: 2025/02/18 00:07:26 by jeflores         ###   ########.fr       */
+/*   Updated: 2025/02/18 22:57:09 by jeflores         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,25 +15,26 @@
 static	int	tokens_counter(const char *s, char delim)
 {
 	int tokens;
-	int in_word;
+	int in_token;
 
 	tokens = 0;
-	in_word = 0;
+	in_token = 0;
 	while (*s)
 	{
-		if (*s != delim && in_word == 0)
+		if (*s != delim && in_token == 0)
 		{
-			in_word = 1;
+			in_token = 1;
 			tokens++;
 		}
 		else if (*s == delim)
 		{
-			in_word = 0;
+			in_token = 0;
 		}
 		s++;
 	}
+	return (tokens);
 }
-static int token_copy(const char* s, int start, int end)
+static char *token_copy(const char* s, int start, int end)
 {
 	int len;
 	int i;
@@ -43,6 +44,7 @@ static int token_copy(const char* s, int start, int end)
 	token = (char *)malloc(sizeof(char) * (len + 1));
 	if (!token)
 		return (NULL);
+	i = 0;
 	while (i < len)
 	{
 		token[i] = s[start + i];
@@ -52,11 +54,12 @@ static int token_copy(const char* s, int start, int end)
 	return (token);
 }
 
-char **ft_split(const char *s, char *c)
+char **ft_split(const char *s, char c)
 {
 	char** result;
 	int total_tokens;
 	int i;
+	int j;
 	int arr_index;
 	int start_index;
 
@@ -72,29 +75,56 @@ char **ft_split(const char *s, char *c)
 	result = (char**)malloc(sizeof(char *) * (total_tokens + 1));
 	if (!result)
 		return (NULL);
-	
-
+	i = 0;
+	arr_index = 0;
+	while (s[i])
+	{	
+		while (s[i] == c && s[i])
+		{
+			i++;
+		}
+		if (s[i] != c)
+		{
+			start_index = i;
+			while (s[i] != c && s[i])
+				i++;
+			result[arr_index] = token_copy(s, start_index, i);
+			if (!result[arr_index])
+			{
+				j = 0;
+				while (j < arr_index)
+				{
+					free(result[j]);
+				}
+				free(result);
+				return (NULL);
+			}
+			arr_index++;
+		}
+	}
+	result[arr_index] = NULL;
+	return (result);
 }
 
-/* int main(void)
+int main(void)
 {
 	int i = 0;
-    char **split_result;
-    char *cadena = " Hola   mundo,   esto es  ft_split ";
-    char delimitador = ' ';
+    char **result;
+    char *str = " Hola   xxxx,   pikachu aprende  ft_split ";
+    char delim = ' ';
 
-    split_result = ft_split(cadena, delimitador);
-    if (!split_result)
-        return 1;
+    result = ft_split(str, delim);
+    if (!result)
+        return (1);
 
     // Imprimimos cada palabra resultante en una línea
-    while (split_result[i] != NULL)
+    while (result[i])
     {
-        printf("Palabra %d: \"%s\"\n", i, split_result[i]);
-        free(split_result[i]);  // Liberamos cada palabra
+        printf("Token [%d]: \"%s\"\n", i, result[i]);
+        free(result[i]);  // Liberamos cada token
 		i++;
     }
-    free(split_result);         // Liberamos el array de punteros
+    free(result);         // Liberamos el array de punteros
 
-    return 0;
-} */
+    return (0);
+}
