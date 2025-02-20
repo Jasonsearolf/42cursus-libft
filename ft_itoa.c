e2889dd87d4634db8f6408615e5a204329ea222c
Jason Searolf <jason@adatier.com>
2025-02-21 00:45:48 +0100

    itoa done

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,38 +6,68 @@
 /*   By: jeflores <jeflores@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/19 15:00:21 by jeflores          #+#    #+#             */
-/*   Updated: 2025/02/19 15:00:21 by jeflores         ###   ########.fr       */
+/*   Updated: 2025/02/21 00:39:02 by jeflores         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+static int	ft_int_len(int n)
+{
+	long	num;
+	int		len;
+
+	num = n;
+	if (num == 0)
+		return (1);
+	len = 0;
+	if (num < 0)
+	{
+		num *= -1;
+		len++;
+	}
+	while (num > 0)
+	{
+		num /= 10;
+		len++;
+	}
+	return (len);
+}
+
 char	*ft_itoa(int n)
 {
-	int i;
-	size_t len;
-	char *result;
-	const unsigned char* str;
+	char	*str;
+	long	num;
+	int		int_len;
 
-	i = 0;
-	str = (const unsigned char*)n;
-	len = ft_strlen(str);
-	result = (char *)malloc(sizeof(char) * (len + 1));
-	while (str != '\0')
+	num = n;
+	int_len = ft_int_len(n);
+	str = (char *)malloc(sizeof(char) * (int_len + 1));
+	if (!str)
+		return (NULL);
+	str[int_len--] = '\0';
+	if (num == 0)
+		str[0] = '0';
+	else if (num < 0)
 	{
-		result[i] = str[i];
-		i++;
+		str[0] = '-';
+		num *= -1;
 	}
-	return (result);
-
+	while (num > 0)
+	{
+		str[int_len] = ((num % 10) + '0');
+		num /= 10;
+		int_len--;
+	}
+	return (str);
 }
 
 int main(void)
 {
-	int num = 1234;
-	char * result;
+	int num = -2147483648;
+	char *result;
 	result = ft_itoa(num);
 
-	printf("%s", result);
+	printf("%s\n", result);
 	return (0);
 }
